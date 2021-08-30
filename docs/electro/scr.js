19e7b9ce3caa2d48d1bf9095f77015c2814f4724
Раиль Рафиков <rayraf@LIVE.RU>
2021-08-30 18:47:26 +0500

    added jquery

--- docs/electro/scr.js
+++ docs/electro/scr.js
@@ -1,4 +1,4 @@
-( document ).ready(function() {
+$( document ).ready(function() {
     $('.trigger').click(function() {
        $('.modal-wrapper').toggleClass('open');
       $('.page-wrapper').toggleClass('blur');
