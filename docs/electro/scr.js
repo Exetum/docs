9b2195fab6447fca1fbfc9d9f5dba4336ba0e1c7
Раиль Рафиков <rayraf@LIVE.RU>
2021-09-06 23:05:24 +0500

    Создана предварительная версия страницы справки

--- docs/electro/scr.js
+++ docs/electro/scr.js
@@ -1,17 +1,156 @@
+
+
+function yourFunction() {
+  
+    var m = document.querySelector('#cnt');
+     if (m != null)
+     {
+      m.remove();   
+     }
+    var cloned_cont = document.getElementById('ct1').cloneNode(true);
+    cloned_cont.id = 'cnt';                      
+    document.querySelector('#fp').appendChild(cloned_cont);
+     
+};
+
+
+function yourFunction2() {
+  
+
+  var m = document.querySelector('#cnt');
+  if (m != null)
+  {
+    m.remove();   
+  }
+
+  // document.querySelector('#cnt').remove();
+  var cloned_cont = document.getElementById('ct2').cloneNode(true);
+  cloned_cont.id = 'cnt';                        
+  document.querySelector('#fp').appendChild(cloned_cont);
+
+};
+
+
+function yourFunction3() {
+  
+
+  var m = document.querySelector('#cnt');
+  if (m != null)
+  {
+    m.remove();   
+  }
+
+  // document.querySelector('#cnt').remove();
+  var cloned_cont = document.getElementById('ct3').cloneNode(true);
+  cloned_cont.id = 'cnt';                        
+  document.querySelector('#fp').appendChild(cloned_cont);
+
+};
+
+
+function yourFunction4() {
+  
+
+  var m = document.querySelector('#cnt');
+  if (m != null)
+  {
+    m.remove();   
+  }
+
+  // document.querySelector('#cnt').remove();
+  var cloned_cont = document.getElementById('ct4').cloneNode(true);
+  cloned_cont.id = 'cnt';                        
+  document.querySelector('#fp').appendChild(cloned_cont);
+
+};
+
+function yourFunction5() {
+  
+
+  var m = document.querySelector('#cnt');
+  if (m != null)
+  {
+    m.remove();   
+  }
+
+  // document.querySelector('#cnt').remove();
+  var cloned_cont = document.getElementById('ct5').cloneNode(true);
+  cloned_cont.id = 'cnt';                        
+  document.querySelector('#fp').appendChild(cloned_cont);
+
+};
+
+
+function yourFunction6() {
+  
+
+  var m = document.querySelector('#cnt');
+  if (m != null)
+  {
+    m.remove();   
+  }
+
+  // document.querySelector('#cnt').remove();
+  var cloned_cont = document.getElementById('ct6').cloneNode(true);
+  cloned_cont.id = 'cnt';                        
+  document.querySelector('#fp').appendChild(cloned_cont);
+
+};
+
+function yourFunction7() {
+  
+
+  var m = document.querySelector('#cnt');
+  if (m != null)
+  {
+    m.remove();   
+  }
+
+  // document.querySelector('#cnt').remove();
+  var cloned_cont = document.getElementById('ct7').cloneNode(true);
+  cloned_cont.id = 'cnt';                        
+  document.querySelector('#fp').appendChild(cloned_cont);
+
+};
+
+function yourFunction8() {
+  
+
+  var m = document.querySelector('#cnt');
+  if (m != null)
+  {
+    m.remove();   
+  }
+
+  // document.querySelector('#cnt').remove();
+  var cloned_cont = document.getElementById('ct8').cloneNode(true);
+  cloned_cont.id = 'cnt';                        
+  document.querySelector('#fp').appendChild(cloned_cont);
+
+};
+
+function yourFunction9() {
+  
+
+  var m = document.querySelector('#cnt');
+  if (m != null)
+  {
+    m.remove();   
+  }
+
+  // document.querySelector('#cnt').remove();
+  var cloned_cont = document.getElementById('ct9').cloneNode(true);
+  cloned_cont.id = 'cnt';                        
+  document.querySelector('#fp').appendChild(cloned_cont);
+
+};
+
+
+
 $( document ).ready(function() {
-    $('.trigger').click(function() {
-       $('.modal-wrapper').toggleClass('open');
-      $('.page-wrapper').toggleClass('blur');
-       return false;
-    });
+  $('.trigger').click(function() {
+     $('.modal-wrapper').toggleClass('open');
+    $('.page-wrapper').toggleClass('blur');
+     return false;
   });
-
-//   var button = document.querySelector("button");
-//   button.addEventListener("click", function() {
-//     console.log("Нажата кнопка 2");
-//   });
-
-//   var button = document.querySelector("btn1");
-//   button.addEventListener("click", function() {
-//     console.log("Нажата кнопка 1");
-//   });
+});
