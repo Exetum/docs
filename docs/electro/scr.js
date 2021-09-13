c89b755299edf6b7141f30cacb251b9d4c4668dc
Раиль Рафиков <rayraf@LIVE.RU>
2021-09-13 18:29:38 +0500

    Формат выдачи информации изменен со всплывающего окна на свободную область справа

--- docs/electro/scr.js
+++ docs/electro/scr.js
@@ -1,156 +1,173 @@
 
 
-function yourFunction() {
+function yourFunction(obj) {
   
     var m = document.querySelector('#cnt');
      if (m != null)
      {
       m.remove();   
      }
-    var cloned_cont = document.getElementById('ct1').cloneNode(true);
-    cloned_cont.id = 'cnt';                      
-    document.querySelector('#fp').appendChild(cloned_cont);
+
+     for(let i=0; i<100; i++)
+      {
+        if(obj.id=='btn'+i)
+        {
+          var cloned_cont = document.getElementById('ct'+i).cloneNode(true);
+          cloned_cont.id = 'cnt';
+          cloned_cont.classList.add("foo");                     
+          document.querySelector('#fs').appendChild(cloned_cont);
+        }
+        
+      }
+
+
+    // var cloned_cont = document.getElementById('ct1').cloneNode(true);
+    // cloned_cont.id = 'cnt';                      
+    // document.querySelector('#fp').appendChild(cloned_cont);
      
+    //  alert(obj.id)
+    console.log(obj.id);
+
 };
 
 
-function yourFunction2() {
+// function yourFunction2() {
   
 
-  var m = document.querySelector('#cnt');
-  if (m != null)
-  {
-    m.remove();   
-  }
+//   var m = document.querySelector('#cnt');
+//   if (m != null)
+//   {
+//     m.remove();   
+//   }
 
-  // document.querySelector('#cnt').remove();
-  var cloned_cont = document.getElementById('ct2').cloneNode(true);
-  cloned_cont.id = 'cnt';                        
-  document.querySelector('#fp').appendChild(cloned_cont);
+//   // document.querySelector('#cnt').remove();
+//   var cloned_cont = document.getElementById('ct2').cloneNode(true);
+//   cloned_cont.id = 'cnt';                        
+//   document.querySelector('#fp').appendChild(cloned_cont);
 
-};
+// };
 
 
-function yourFunction3() {
+// function yourFunction3() {
   
 
-  var m = document.querySelector('#cnt');
-  if (m != null)
-  {
-    m.remove();   
-  }
+//   var m = document.querySelector('#cnt');
+//   if (m != null)
+//   {
+//     m.remove();   
+//   }
 
-  // document.querySelector('#cnt').remove();
-  var cloned_cont = document.getElementById('ct3').cloneNode(true);
-  cloned_cont.id = 'cnt';                        
-  document.querySelector('#fp').appendChild(cloned_cont);
+//   // document.querySelector('#cnt').remove();
+//   var cloned_cont = document.getElementById('ct3').cloneNode(true);
+//   cloned_cont.id = 'cnt';                        
+//   document.querySelector('#fp').appendChild(cloned_cont);
 
-};
+// };
 
 
-function yourFunction4() {
+// function yourFunction4() {
   
 
-  var m = document.querySelector('#cnt');
-  if (m != null)
-  {
-    m.remove();   
-  }
+//   var m = document.querySelector('#cnt');
+//   if (m != null)
+//   {
+//     m.remove();   
+//   }
 
-  // document.querySelector('#cnt').remove();
-  var cloned_cont = document.getElementById('ct4').cloneNode(true);
-  cloned_cont.id = 'cnt';                        
-  document.querySelector('#fp').appendChild(cloned_cont);
+//   // document.querySelector('#cnt').remove();
+//   var cloned_cont = document.getElementById('ct4').cloneNode(true);
+//   cloned_cont.id = 'cnt';                        
+//   document.querySelector('#fp').appendChild(cloned_cont);
 
-};
+// };
 
-function yourFunction5() {
+// function yourFunction5() {
   
 
-  var m = document.querySelector('#cnt');
-  if (m != null)
-  {
-    m.remove();   
-  }
+//   var m = document.querySelector('#cnt');
+//   if (m != null)
+//   {
+//     m.remove();   
+//   }
 
-  // document.querySelector('#cnt').remove();
-  var cloned_cont = document.getElementById('ct5').cloneNode(true);
-  cloned_cont.id = 'cnt';                        
-  document.querySelector('#fp').appendChild(cloned_cont);
+//   // document.querySelector('#cnt').remove();
+//   var cloned_cont = document.getElementById('ct5').cloneNode(true);
+//   cloned_cont.id = 'cnt';                        
+//   document.querySelector('#fp').appendChild(cloned_cont);
 
-};
+// };
 
 
-function yourFunction6() {
+// function yourFunction6() {
   
 
-  var m = document.querySelector('#cnt');
-  if (m != null)
-  {
-    m.remove();   
-  }
+//   var m = document.querySelector('#cnt');
+//   if (m != null)
+//   {
+//     m.remove();   
+//   }
 
-  // document.querySelector('#cnt').remove();
-  var cloned_cont = document.getElementById('ct6').cloneNode(true);
-  cloned_cont.id = 'cnt';                        
-  document.querySelector('#fp').appendChild(cloned_cont);
+//   // document.querySelector('#cnt').remove();
+//   var cloned_cont = document.getElementById('ct6').cloneNode(true);
+//   cloned_cont.id = 'cnt';                        
+//   document.querySelector('#fp').appendChild(cloned_cont);
 
-};
+// };
 
-function yourFunction7() {
+// function yourFunction7() {
   
 
-  var m = document.querySelector('#cnt');
-  if (m != null)
-  {
-    m.remove();   
-  }
+//   var m = document.querySelector('#cnt');
+//   if (m != null)
+//   {
+//     m.remove();   
+//   }
 
-  // document.querySelector('#cnt').remove();
-  var cloned_cont = document.getElementById('ct7').cloneNode(true);
-  cloned_cont.id = 'cnt';                        
-  document.querySelector('#fp').appendChild(cloned_cont);
+//   // document.querySelector('#cnt').remove();
+//   var cloned_cont = document.getElementById('ct7').cloneNode(true);
+//   cloned_cont.id = 'cnt';                        
+//   document.querySelector('#fp').appendChild(cloned_cont);
 
-};
+// };
 
-function yourFunction8() {
+// function yourFunction8() {
   
 
-  var m = document.querySelector('#cnt');
-  if (m != null)
-  {
-    m.remove();   
-  }
+//   var m = document.querySelector('#cnt');
+//   if (m != null)
+//   {
+//     m.remove();   
+//   }
 
-  // document.querySelector('#cnt').remove();
-  var cloned_cont = document.getElementById('ct8').cloneNode(true);
-  cloned_cont.id = 'cnt';                        
-  document.querySelector('#fp').appendChild(cloned_cont);
+//   // document.querySelector('#cnt').remove();
+//   var cloned_cont = document.getElementById('ct8').cloneNode(true);
+//   cloned_cont.id = 'cnt';                        
+//   document.querySelector('#fp').appendChild(cloned_cont);
 
-};
+// };
 
-function yourFunction9() {
+// function yourFunction9() {
   
 
-  var m = document.querySelector('#cnt');
-  if (m != null)
-  {
-    m.remove();   
-  }
+//   var m = document.querySelector('#cnt');
+//   if (m != null)
+//   {
+//     m.remove();   
+//   }
 
-  // document.querySelector('#cnt').remove();
-  var cloned_cont = document.getElementById('ct9').cloneNode(true);
-  cloned_cont.id = 'cnt';                        
-  document.querySelector('#fp').appendChild(cloned_cont);
+//   // document.querySelector('#cnt').remove();
+//   var cloned_cont = document.getElementById('ct9').cloneNode(true);
+//   cloned_cont.id = 'cnt';                        
+//   document.querySelector('#fp').appendChild(cloned_cont);
 
-};
+// };
 
 
 
-$( document ).ready(function() {
-  $('.trigger').click(function() {
-     $('.modal-wrapper').toggleClass('open');
-    $('.page-wrapper').toggleClass('blur');
-     return false;
-  });
-});
+// // $( document ).ready(function() {
+// //   $('.trigger').click(function() {
+// //      $('.modal-wrapper').toggleClass('open');
+// //     $('.page-wrapper').toggleClass('blur');
+// //      return false;
+// //   });
+// // });
